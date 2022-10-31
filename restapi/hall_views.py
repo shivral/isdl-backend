75569714abbae93d520419853b859b1424d38ee7
shivral somani <shivral312002@gmail.com>
2022-10-31 21:22:12 +0530

    halls

--- restapi/hall_views.py
+++ restapi/hall_views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from .serializers import HallSerializer
 @api_view(['POST'])
 def createHall(request):
-    hs=HallSerializer(request.data)
+    hs=HallSerializer(data=request.data)
     if hs.is_valid():
         hs.save()
     return Response(hs.data)
