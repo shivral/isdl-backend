4ccde36fcc0227e98fb9630b3cd92225066409d6
shivral somani <shivral312002@gmail.com>
2022-11-01 00:56:35 +0530

    halls

--- restapi/user_views.py
+++ restapi/user_views.py
@@ -1,20 +1,21 @@
 from .views import authuser,Decode
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import LectureHall
+from .models import Hall, LectureHall
 from .models import Booking
-from .serializers import BookingSerializer
+from .serializers import BookingSerializer,HallSerializer
 import datetime
 
 @api_view(['POST','GET'])
 def createBooking(request):
     user=authuser(request)
-    print(user)
     if user:
         hallid = request.query_params.get('hall_id',None)
         hall = LectureHall.objects.all().first()
         Booking.objects.create(actor=user,hall=hall,booked=False,pending=True,slotStart=str(datetime.datetime.now()),slotEnd=str(datetime.datetime.now()))                    
         print(Booking.objects.all())
-        return Response({'Data':'Success'})
+        hs=HallSerializer(hall)
+        if hs.is_valid():
+            return Response(hs.data)
     else:
         return Response({'Data':'Failed'})
