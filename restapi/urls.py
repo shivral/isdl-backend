ed19093657c45751b18926299dbd9419cee79184
shivral somani <shivral312002@gmail.com>
2022-10-31 21:16:13 +0530

    halls

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,re_path
 from .views import api_home, logout,register,login, userView
 from .user_views import createBooking
+from .hall_views import createHall
 urlpatterns = [
     path('home',api_home),
     path('register',register),
     path('login',login),
     path('user',userView),
     path('logout',logout),
-    path('createBooking',createBooking)
+    path('createBooking',createBooking),
+    path('createHall',createHall)
 ]
