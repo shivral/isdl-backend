f66fba9e9d4ea24bafad16f3087d137a409d8722
shivral somani <shivral312002@gmail.com>
2022-11-01 10:02:28 +0530

    admin

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from .views import api_home, logout,register,login, userView,getAllHalls
 from .user_views import createBooking,getUserBooking
 from .hall_views import createHall
-from .admin_views import registerAdmin,loginAdmin,getAllPending
+from .admin_views import acceptRequest, registerAdmin,loginAdmin,getAllPending
 urlpatterns = [
     path('home',api_home),
     path('register',register),
@@ -15,5 +15,6 @@ urlpatterns = [
     path('getUserBookings',getUserBooking),
     path('registerAdmin',registerAdmin),
     path('loginAdmin',loginAdmin),
-    path('getAllPending',getAllPending)
+    path('getAllPending',getAllPending),
+    path('acceptRequest',acceptRequest)
 ]
