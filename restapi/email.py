56dc80701d91525cdf6c191f06c4956dc6325b13
shivral somani <shivral312002@gmail.com>
2022-11-05 15:22:46 +0530

    Res

--- restapi/email.py
+++ restapi/email.py
@@ -5,14 +5,14 @@ i=0
 body=""    #body of the mail
 def sendMail(body,toaddr):
     msg=MIMEText(body)
-    fromaddr= "shivral312002@gmail.com" #the adress from which mail is sent # give acess to less secure apps
+    fromaddr= "swivral312002@gmail.com" #the adress from which mail is sent # give acess to less secure apps
     # toaddr=""  # mail sent to
     msg["From"]=fromaddr     #### store the adresses into msg object
     msg["To"]=toaddr
     msg["Subject"]="Complaint"
     server=smtplib.SMTP("smtp.gmail.com",587)   ###connect to gmail.com server using 587 port number
     server.starttls()                   #put the smtp connection in TLS mode
-    server.login(fromaddr,"kdihrqnpsgizcddu")  #login to your the server with the correct password
+    server.login(fromaddr,"nmdhzzakcthaztan")  #login to your the server with the correct password
     server.send_message(msg) #send the message to the server
     print("mail sent")
     server.quit() #close the connection
